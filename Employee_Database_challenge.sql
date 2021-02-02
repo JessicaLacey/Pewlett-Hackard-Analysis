93e0cce50578fff9e46291bbc30d2626c86627ad
Jessica Lacey <74221473+JessicaLacey@users.noreply.github.com>
2021-02-01 21:00:36 -0600

    Updating Employee_Database_challenge.sql

--- Employee_Database_challenge.sql
+++ Employee_Database_challenge.sql
@@ -47,3 +47,33 @@ GROUP BY unique_titles.title
 ORDER BY count DESC;
 
 SELECT * FROM retiring_titles;
+
+-- Retrieve data
+SELECT emp_no, first_name, last_name, birth_date
+FROM employees
+
+SELECT from_date, to_date
+FROM dept_emp
+
+SELECT title
+FROM titles
+
+-- Use a DISTINCT ON statement to retrieve the first occurrence of the employee number for each set of rows
+-- Create a new table for Mentorship Eligibility
+SELECT DISTINCT ON (employees.emp_no) employees.emp_no,
+	employees.first_name,
+	employees.last_name,
+	employees.birth_date,
+	dept_emp.from_date,
+	dept_emp.to_date,
+	titles.title
+INTO mentorship_eligibility
+FROM employees
+INNER JOIN dept_emp
+ON (employees.emp_no = dept_emp.emp_no)
+INNER JOIN titles
+ON (employees.emp_no = titles.emp_no)
+WHERE (employees.birth_date BETWEEN '1965-01-01' AND '1965-12-31')
+ORDER BY emp_no;
+
+SELECT * FROM mentorship_eligibility;
